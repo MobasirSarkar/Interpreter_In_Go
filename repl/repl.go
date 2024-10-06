0e00a0893e0e6637d725d8e6c109d4a17bcdf80a
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-10-06 13:09:10 +0530

    completed the eval BangOperator

--- repl/repl.go
+++ repl/repl.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/MobasirSarkar/Interpreter_In_Go/evaluator"
 	"github.com/MobasirSarkar/Interpreter_In_Go/lexer"
-	"github.com/MobasirSarkar/Interpreter_In_Go/token"
+	"github.com/MobasirSarkar/Interpreter_In_Go/parser"
 )
 
 const PROMPT = ">>"
@@ -14,15 +15,40 @@ const PROMPT = ">>"
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	for {
-		fmt.Println(PROMPT)
+		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+const MOBASIR_SARKAR = `
+    /\      |\    /|  
+   /__\     | \  / |  
+  /    \    |  \/  |  
+   `
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MOBASIR_SARKAR)
+	io.WriteString(out, "Oops ! We ran into Some Problem!\n")
+	io.WriteString(out, "parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
